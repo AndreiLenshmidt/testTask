22428255870b5475ba6d41f9d02718f457a82466
Lenshmidt Andrei <lenshmidt.andrei@gmail.com>
2024-09-19 15:18:07 +0300

    change navigator

--- modules/navigator.js
+++ modules/navigator.js
@@ -1,12 +1,7 @@
 import { showMessage } from "./modal.js";
 
 export function findLocation(loadWeather) {
-  if (!navigator.geolocation) {
-    showMessage("Ошибка", "Не удалось определить геолокацию...");
-    loadWeather(39.02, 45.06);
-  } else {
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
+  navigator.geolocation.getCurrentPosition(success, error);
 
   async function success(position) {
     const { longitude, latitude } = position.coords;
@@ -21,7 +16,7 @@ export function findLocation(loadWeather) {
   }
 
   function error() {
-    console.log("Не получается определить вашу геолокацию :(");
-    return new Error("Не получается определить вашу геолокацию :(");
+    showMessage("Ваш город Краснодар?", "Не удалось определить геолокацию...");
+    loadWeather(39.02, 45.06);
   }
 }
